c3f40682865ef436a279026ddae4e29b47a8c1e8
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-29 15:20:56 +0530

    Created models And add user,feature,permission,role in go

--- models/user.go
+++ models/user.go
@@ -1,17 +1,23 @@
 package models
 
+import (
+	"webgolang/database"
+)
+
 type User  struct {
 	ID     int
 	Name   string
 	Email  string
 	RoleID int
-	Role   Role
 }
 
 func GetUserByID(id int) (*User, error) {
+	row := database.DB.QueryRow("SELECT id, name, email, role_id FROM users WHERE id=$1", id)
 
-}
-
-func CreateUser(user User) error {
-	
+	var user User
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.RoleID)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
