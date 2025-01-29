2500b74bcd5927ef4d56a5d5144ee6e359617974
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-29 15:16:01 +0530

    Created database and add db.go

---
+++ database/db.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	connStr := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Database unreachable: ", err)
+	}
+
+	log.Println("Database connected successfully")
+}
